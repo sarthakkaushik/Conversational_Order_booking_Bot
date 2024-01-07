19ffa710a3d591fb19ee3990e1399e29ded9200c
sarthakkaushik <sarthak.kaushik.17@gmail.com>
2024-01-07 16:33:10 +0530

    latest changes

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "IOCL_BU_Python_Scripts"
+project_name = "Conversational_Order_booking"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
